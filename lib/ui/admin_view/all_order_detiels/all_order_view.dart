02a537a8d7db7acd89ad31617a817f0cf0e80a2a
laith-alskaf <liethmhmad231@gmail.com>
2024-06-22 01:41:30 +0300

    s

--- lib/ui/admin_view/all_order_detiels/all_order_view.dart
+++ lib/ui/admin_view/all_order_detiels/all_order_view.dart
@@ -13,117 +13,119 @@ import 'package:get/get.dart';
 import 'all_order_controller.dart';
 
 class AllOrderView extends StatelessWidget {
-  const AllOrderView({super.key});
+   AllOrderView({super.key});
+  AllOrderController controller = Get.put(AllOrderController());
 
   @override
   Widget build(BuildContext context) {
-    AllOrderController controller = Get.put(AllOrderController());
-    return ListView(children: [
-      const CustomAppBar(
-        title: 'All Orders',
-        iconSetting: true,
-      ),
-      Padding(
-          padding:
-              EdgeInsets.only(right: 30.w, left: 30.w, bottom: 10.h, top: 20.h),
-          child: GetBuilder<AllOrderController>(builder: (s) {
-            return Container(
-              width: 516.w,
-              height: 60.h,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
-                    spreadRadius: 2,
-                    blurRadius: 3,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-                border: Border.all(color: AppColors.whiteColor),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(25.r),
-                ),
-                color: AppColors.whiteColor,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // ServiceTitle(
-                  //   onTap: () {
-                  //     controller.handleClickFilter(serviceIndex: 0);
-                  //   },
-                  //   onClick: controller.index == 0 ? true : false,
-                  //   selectedContainer: (controller.index == 0)
-                  //       ? AppColors.mainColor
-                  //       : Colors.transparent,
-                  //   title: 'Parking',
-                  //   titleContainer: (controller.index == 0)
-                  //       ? AppColors.whiteColor
-                  //       : AppColors.colorBorder,
-                  // ),
-                  ServiceTitle(
-                    onTap: () {
-                      controller.handleClickFilter(serviceIndex: 1);
-                    },
-                    onClick: controller.index == 1 ? true : false,
-                    selectedContainer: (controller.index == 1)
-                        ? AppColors.mainColor
-                        : Colors.transparent,
-                    title: 'Repairs',
-                    titleContainer: (controller.index == 1)
-                        ? AppColors.whiteColor
-                        : AppColors.colorBorder,
-                  ),
-                ],
-              ),
-            );
-          })),
-      ZoomIn(
-        child: SizedBox(
-          height: 1.sh,
-          child: Padding(
-              padding: EdgeInsets.only(top: 10.h, bottom: 290.h),
-              child: GetBuilder<HistoryViewController>(
-                builder: (c) {
-                  return
-                  //   controller.index == 0
-                  //     ? (controller.parkingHistory == null
-                  //     ? Center(
-                  //   child: SpinKitCircle(
-                  //     color: AppColors.mainColor,
-                  //     size: 100.w,
-                  //   ),
-                  // )
-                  //     : controller.parkingHistory!.isEmpty
-                  //     ? const Center(
-                  //   child: CustomText(
-                  //       text: 'No Data',
-                  //       textType: TextStyleType.title),
-                  // )
-                  //     : ContainerHistoryQr(
-                  //   onTap: () {},
-                  // ))
-                  //     :
-                  (controller.problemHistory == null
-                      ? Center(
-                    child: SpinKitCircle(
-                      color: AppColors.mainColor,
-                      size: 100.w,
-                    ),
-                  )
-                      : controller.problemHistory!.isEmpty
-                      ? const Center(
-                    child: CustomText(
-                        text: 'No Data',
-                        textType: TextStyleType.title),
-                  )
-                      : ContainerHistoryQr(
-                    onTap: () {},
-                  ));
-                },
-              )),
+    return SafeArea(
+      child: Scaffold(backgroundColor: AppColors.whiteColor,body: ListView(children: [
+        const CustomAppBar(
+          title: 'All Orders',
+          iconSetting: true,
         ),
-      )
-    ]);
+        Padding(
+            padding:
+            EdgeInsets.only(right: 30.w, left: 30.w, bottom: 10.h, top: 20.h),
+            child: GetBuilder<AllOrderController>(builder: (s) {
+              return Container(
+                width: 516.w,
+                height: 60.h,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      spreadRadius: 2,
+                      blurRadius: 3,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                  border: Border.all(color: AppColors.whiteColor),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25.r),
+                  ),
+                  color: AppColors.whiteColor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ServiceTitle(
+                      onTap: () {
+                        controller.handleClickFilter(serviceIndex: 0);
+                      },
+                      onClick: controller.index == 0 ? true : false,
+                      selectedContainer: (controller.index == 0)
+                          ? AppColors.mainColor
+                          : Colors.transparent,
+                      title: 'Parking',
+                      titleContainer: (controller.index == 0)
+                          ? AppColors.whiteColor
+                          : AppColors.colorBorder,
+                    ),
+                    ServiceTitle(
+                      onTap: () {
+                        controller.handleClickFilter(serviceIndex: 1);
+                      },
+                      onClick: controller.index == 1 ? true : false,
+                      selectedContainer: (controller.index == 1)
+                          ? AppColors.mainColor
+                          : Colors.transparent,
+                      title: 'Repairs',
+                      titleContainer: (controller.index == 1)
+                          ? AppColors.whiteColor
+                          : AppColors.colorBorder,
+                    ),
+                  ],
+                ),
+              );
+            })),
+        ZoomIn(
+          child: SizedBox(
+            height: 1.sh,
+            child: Padding(
+                padding: EdgeInsets.only(top: 10.h, bottom: 290.h),
+                child: GetBuilder<AllOrderController>(
+                  builder: (c) {
+                    return
+                      controller.index == 0
+                          ? (controller.parkingHistory == null
+                          ? Center(
+                        child: SpinKitCircle(
+                          color: AppColors.mainColor,
+                          size: 100.w,
+                        ),
+                      )
+                          : controller.parkingHistory!.isEmpty
+                          ? const Center(
+                        child: CustomText(
+                            text: 'No Data',
+                            textType: TextStyleType.title),
+                      )
+                          : ContainerHistoryQr(
+                        onTap: () {},
+                      ))
+                          :
+                      (controller.problemHistory == null
+                          ? Center(
+                        child: SpinKitCircle(
+                          color: AppColors.mainColor,
+                          size: 100.w,
+                        ),
+                      )
+                          : controller.problemHistory!.isEmpty
+                          ? const Center(
+                        child: CustomText(
+                            text: 'No Data',
+                            textType: TextStyleType.title),
+                      )
+                          : ContainerHistoryQr(
+                        onTap: () {},
+                      ));
+                  },
+                )),
+          ),
+        )
+      ]),)
+    );
   }
 }
